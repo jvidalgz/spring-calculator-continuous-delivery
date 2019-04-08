5b09f6d59700d050d6e58d0c9fb4069aad899b3e
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2019-04-08 10:59:58 -0400

    Prepara para tests

--- src/test/java/com/jaime/calculator/CalculatorTest.java
+++ src/test/java/com/jaime/calculator/CalculatorTest.java
@@ -4,11 +4,17 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Calculator test.
+ */
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
 
     @Test
     public void testSum() {
-        assertEquals(5, calculator.sum(2, 3));
+        assertEquals(5, calculator.sum(2
+        /**
+ * Calculator test.
+ */, 3));
     }
 }
